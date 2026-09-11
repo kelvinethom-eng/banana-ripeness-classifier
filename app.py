5513821984ff476541c7c5efa4a5d98804f70db1
kelvinethom-eng <kelvinethom@gmail.com>
2026-09-11 17:43:47 +0700

    Reformat app.py with consistent import statements

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess & predict
     img_resized = image.resize((224, 224))
